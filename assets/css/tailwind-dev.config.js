eb3ddf4ed425334f9e4d24795f68bc9c565c12a2
murshidazher <murshid@different.com.au>
2022-01-19 13:49:38 +0530

    :sparkles: feat: update tailwind dev config

--- assets/css/tailwind-dev.config.js
+++ assets/css/tailwind-dev.config.js
@@ -1,6 +1,16 @@
+const themeDir = __dirname + "/../../";
 const defaultTheme = require("tailwindcss/defaultTheme");
 const neColors = require("./tailwind-colors"); // ne stands for northeast;
 module.exports = {
+    content: [
+        themeDir + "layouts/**/*.html",
+        themeDir + "content/**/*.html",
+        themeDir + "content/**/*.html",
+        "themes/murshid-starter/layouts/**/*.html",
+        "content/**/*.html",
+        "exampleSite/layouts/**/*.html",
+        "exampleSite/content/**/*.html",
+    ],
     theme: {
         extend: {
             fontFamily: {
