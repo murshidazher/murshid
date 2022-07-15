9a340e9d257636213c7d70cd3deac853b6767528
murshidazher <murshid@different.com.au>
2022-07-15 16:26:19 +0530

    feat: Add hugo title bar

--- assets/css/tailwind-dev.config.js
+++ assets/css/tailwind-dev.config.js
@@ -2,11 +2,12 @@ const themeDir = __dirname + "/../../";
 const defaultTheme = require("tailwindcss/defaultTheme");
 const neColors = require("./tailwind-colors"); // ne stands for northeast;
 module.exports = {
+    darkMode: 'class',
     content: [
         themeDir + "layouts/**/*.html",
         themeDir + "content/**/*.html",
         themeDir + "content/**/*.html",
-        "node_modules/preline/dist/*.js",
+        "node_modules/preline/dist/**/*.js",
         "themes/murshid-starter/layouts/**/*.html",
         "content/**/*.html",
         "exampleSite/layouts/**/*.html",
--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -2,10 +2,11 @@ const themeDir = __dirname + "/../../";
 const defaultTheme = require("tailwindcss/defaultTheme");
 const neColors = require("./tailwind-colors"); // ne stands for northeast;
 module.exports = {
+    darkMode: 'class',
     content: [
         themeDir + "layouts/**/*.html",
         themeDir + "content/**/*.html",
-        "node_modules/preline/dist/*.js",
+        "node_modules/preline/dist/**/*.js",
         "layouts/**/*.html",
         "content/**/*.html",
         "exampleSite/layouts/**/*.html",
