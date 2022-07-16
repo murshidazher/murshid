2fb0ee2034313fb48470da9d61eeb7f4e67387cc
murshidazher <murshid@different.com.au>
2022-07-16 19:56:15 +0530

    feat: Add onscroll header dynamic blur

--- assets/js/shortcuts.js
+++ assets/js/shortcuts.js
@@ -1,8 +1,27 @@
 // Define short of 'a'
-key('⌘+k, ctrl+k', () => { let modalEl = document.getElementById('global-search-modal');  HSModal.toggle(modalEl) });
+key('⌘+k, ctrl+k', () => {
+    let modalEl = document.getElementById('global-search-modal');
+    HSModal.toggle(modalEl)
+});
 
 // Returning false stops the event and prevents default browser events
-key('ctrl+r', function(){ alert('stopped reload!'); return false });
+key('ctrl+r', () => {
+    alert('stopped reload!');
+    return false
+});
 
 // Multiple shortcuts that do the same thing
-key('⌘+r, ctrl+r', function(){ });
+key('⌘+r, ctrl+r', () => {});
+
+// TODO: move to a sepeate file (header blur)
+window.addEventListener('scroll', event => {
+    const height = 40;
+    const {
+        scrollTop
+    } = event.target.scrollingElement;
+    const header = document.getElementById('main-header');
+    header.classList.toggle('backdrop-blur', scrollTop >= height);
+    header.classList.toggle('border-b', scrollTop >= height);
+    header.classList.toggle('border-slate-900/10', scrollTop >= height);
+
+});
